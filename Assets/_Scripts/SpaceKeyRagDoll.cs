3dcbd46e5e83adfc72469c1c841909e10dae7e74
dr-matt-smith <matt.smith@itb.ie>
2020-11-30 18:48:19 +0000

    added more files

---
+++ Assets/_Scripts/SpaceKeyRagDoll.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UMA.Dynamics;
+
+public class SpaceKeyRagDoll : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            MakeRagDoll();
+        }
+    }
+    
+    private void MakeRagDoll()
+    {
+        Transform avatar = GetComponent<Rigidbody>().transform.root;
+        UMAPhysicsAvatar physicsAvatar = avatar.GetComponent<UMAPhysicsAvatar>();
+        physicsAvatar.ragdolled = true;
+   }
+}
